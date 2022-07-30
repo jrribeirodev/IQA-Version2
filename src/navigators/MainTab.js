e0de23c87cb9338dd547fc8ca8cabcc02efaa25a
Junior Ribeiro <42409117+jrribeirodev@users.noreply.github.com>
2022-07-30 13:16:35 -0300

    Inserção dos icones no tabBar

--- src/navigators/MainTab.js
+++ src/navigators/MainTab.js
@@ -2,11 +2,30 @@ import React from "react";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import TabHomeScreen from '../screens/Home'
 import TabFindScreen from '../screens/Find'
+import {Image} from 'react-native'
 
 const Tab = createBottomTabNavigator();
 export default () => (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Home" component={TabHomeScreen}/>
-        <Tab.Screen name="Find" component={TabFindScreen}/>
+    <Tab.Navigator
+     
+        screenOptions={({route})=>({
+        tabBarIcon:()=>{
+            let iconSource = null;
+            switch(route.name){
+                case 'Home':
+                    iconSource=require('../assets/home.png');
+                    break;
+                    case 'Search':
+                        iconSource=require('../assets/search.png');
+                    break;                  
+            }
+            return <Image source={iconSource} style={{width:30,height:30}}/>
+        }
+    })
+    
+    }
+    >
+        <Tab.Screen name="Home" component={TabHomeScreen} options={{headerShown: false}}/>
+        <Tab.Screen name="Search" component={TabFindScreen} options={{headerShown: false}}/>
     </Tab.Navigator>
 )
